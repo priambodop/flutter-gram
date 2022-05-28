48e296477b32ef7b04a3fbd1ab1232f5d6a84491
priambodop <priambodop>
2022-05-28 18:22:27 +0700

    first commit to many lets get it

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gram/utils/colors.dart';
 import 'package:flutter_gram/widgets/text_field_input.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +47,43 @@ class LoginScreen extends StatelessWidget {
                 height: 64,
               ),
               // text field for email
+              TextFieldInput(
+                  textEditingController: _emailController,
+                  hintText: 'Enter email address...',
+                  textInputType: TextInputType.emailAddress),
+
+              const SizedBox(height: 24),
 
               // text field for password
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: 'Enter your password...',
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+
+              const SizedBox(height: 24),
+
               // button login
+              Container(
+                child: const Text('Log In'),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  color: blueColor,
+                ),
+              ),
+
+              const SizedBox(height: 12),
+
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+
               // Tansitioning to sign up page
             ],
           ),
